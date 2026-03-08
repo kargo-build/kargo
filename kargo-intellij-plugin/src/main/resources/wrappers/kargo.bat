49bb7784e030bcdb37434545cd3d8679f72a4cc7
Leo Padilha <leodouglas@gmail.com>
2026-03-08 00:00:16 -0300

    feat(intellij-plugin): kargo wizard templates

--- kargo-intellij-plugin/src/main/resources/wrappers/kargo.bat
+++ kargo-intellij-plugin/src/main/resources/wrappers/kargo.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Kargo distribution to provision and use
-set amper_version=0.10.0-dev-3680
+set amper_version=0.1.1
 @rem Establish chain of trust from here by specifying exact checksum of Kargo distribution to be run
-set amper_sha256=61d085f4203a04a8e769adf777c15421fe32e1d6633919a79ce70cb3f49e249a
+set amper_sha256=b789f382a6ccaf6c5d0a3a7563afac7f509cf23328c7a1fe620cee58072b9fd5
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://github.com/kargo-build/kargo/releases/download
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
