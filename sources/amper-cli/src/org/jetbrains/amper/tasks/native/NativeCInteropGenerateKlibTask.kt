73493258aeebcf92305bc18c55a4846019c1734b
Stanislav Utikeev <stanislav.utikeev@jetbrains.com>
2026-04-30 22:16:36 +0200

    [cli] Fix the key for cinterop incremental cache tasks
    We provide incrementality per a .def file, but they share the same key, effectively having no cache in case when there is more than a single definition file. With this change the incremental cache key is extended with the def file name (which should be unique within the fragment because the cinterop file layout is flat).

--- sources/amper-cli/src/org/jetbrains/amper/tasks/native/NativeCInteropGenerateKlibTask.kt
+++ sources/amper-cli/src/org/jetbrains/amper/tasks/native/NativeCInteropGenerateKlibTask.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.amper.tasks.native
@@ -88,7 +88,7 @@ internal class NativeCInteropGenerateKlibTask(
             val relevantOutputs = inputDefFiles.map { defFile ->
                 async {
                     incrementalCache.execute(
-                        key = taskName.name,
+                        key = "${taskName.name}-${defFile.nameWithoutExtension}",
                         inputValues = mapOf(
                             "target" to platform.nameForCompiler,
                             "kotlinVersion" to kotlinCompilerVersion,
